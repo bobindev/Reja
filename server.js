79640999b950346d214eaefd71592cf771fa9e67
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-02-05 22:09:37 +0900

    feat: embodied js

--- server.js
+++ server.js
@@ -4,29 +4,28 @@ const express = require('express');
 const app = express();
 const http = require('http')
 
-//1 - express ga kirib kelishga bog'liq kodlar
+//1 Kirish code -  express ga kirib kelishga bog'liq kodlar
 app.use(express.static("public"));
 app.use(express.json()); // kirib kelayotgan json formatdagi data ni object holatida bizga o'girib beradi
 app.use(express.urlencoded({ extended: true })); //html formdan post qilingan narsalarni express server qabul qilishi un xizmat qiladi
 
 //2: Session
 
-//3 ejs- backendda frontendni yasash uchun xizmat qiladigan muhit  (npm i ejs). View codes
+//3 View codes (ejs- backendda frontendni yasash uchun xizmat qiladigan muhit  (npm i ejs)). 
 
 app.set("views", "views");
 app.set('view engine', 'ejs');
 
 //4 - Routing codes
-app.get("/hello", function (req, res) {
-    res.end(`<h1>HELLO WORLD by BobTech</h1>`)
-});
-
-app.get("/gift", function (req, res) {
-    res.end(`<h1>You are at gifts department !</h1>`);
-});
-
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success" });
+})
+app.get("/", function (req, res) {
+    res.render("harid");
+})
 const server = http.createServer(app);
-let PORT = 3000;
+let PORT = 3003;
 server.listen(PORT, function () {
     console.log(`The server is running successfullu on port: ${PORT}`);
 });
