c37ac6fe8bdf4ca36c3435e4edfef47940e06425
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-02-08 07:46:52 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ console.log("Starting Web Server");
 
 const express = require('express');
 const app = express();
-const http = require('http')
+const http = require('http');
+const fs = require("fs");
 
+let user;
+fs.readFile('database/user.json', "utf8", (err, data) => {
+    if (err) {
+        console.log("ERROR: ", err);
+    } else {
+        user = JSON.parse(data);
+    }
+})
 //1 Kirish code -  express ga kirib kelishga bog'liq kodlar
 app.use(express.static("public"));
 app.use(express.json()); // kirib kelayotgan json formatdagi data ni object holatida bizga o'girib beradi
@@ -18,14 +27,20 @@ app.set('view engine', 'ejs');
 
 //4 - Routing codes
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({ test: "success" });
+    // console.log(req.body);
+    // res.json({ test: "success" });
+    // code with db here
+});
+
+app.get("/author", (req, res) => {
+    res.render("author", { user: user })
 })
 app.get("/", function (req, res) {
     res.render("harid");
+    //res.end("<h1>Hello World!</h1>");
 })
 const server = http.createServer(app);
-let PORT = 3003;
+let PORT = 3000;
 server.listen(PORT, function () {
     console.log(`The server is running successfullu on port: ${PORT}`);
 });
