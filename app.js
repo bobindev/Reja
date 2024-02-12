dc12a5a505b867b4533012681be4557d24c63675
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-02-12 21:31:36 +0900

    feat: CRUD operatsiyalari

--- app.js
+++ app.js
@@ -30,17 +30,38 @@ app.set('view engine', 'ejs');
 
 //4 - Routing codes
 app.post("/create-item", (req, res) => {
-    // console.log(req.body);
-    // res.json({ test: "success" });
+    console.log("user entered / creat item");
+    console.log(req.body);
+    const new_reja = req.body.reja
+    db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end('something went wrong');
+        } else {
+            res.end('successfully added');
+        }
+    });
     // code with db here
 });
 
 app.get("/author", (req, res) => {
+    
     res.render("author", { user: user })
 })
 app.get("/", function (req, res) {
-    res.render("reja");
-    //res.end("<h1>Hello World!</h1>");
+    console.log("user entered /");
+    db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+        if (err) {
+            console.log(err);
+            res.end("Something went wrong!");
+        } else {
+
+            res.render("reja", { items: data });
+        }
+    });
+
 });
 
 module.exports = app;
--- train.js
+++ train.js
@@ -1,59 +1,72 @@
 console.log('TRAIN AREA')
-/*
-//A-TASK: 
+//TASK-B
 
-Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
-MASALAN countLetter("e", "engineer") 3ni return qiladi.
 
-1. engineer => forEach || map || for
-2. condition malumotlarni solishtirish
-3. return qilish
-4. result
 
-//SOLUTION
-function countLetter(letter, word) {
-    let count = 0;
-    for (let i = 0; i < word.length; i++) {
-        if (word.charAt(i) === letter) {
-            count++;
-        }
-    } return count;
-}
-const result = countLetter("e", "engineer");
-console.log(result); // Output : 3
-*/
 
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yaxshi talaba boling", //0-20
-    "togri boshliq tanlang va koproq xato qiling", //20-30
-    "ozingizga ishlashni boshlang", //30-40
-    "siz kuchli bolgan narsalarni qiling", //50-60
-    "endi dam oling, foydasi yoq"
-];
 
-//CALLBACK
-function maslahatBering(a, callback) {
-    if (typeof a !== 'number') callback("insert a number", null);
-    else if (a <= 20) callback(null, list[0]);
-    else if (a > 20 & a <= 30) callback(null, list[1]);
-    else if (a > 30 & a <= 40) callback(null, list[2]);
-    else if (a > 40 & a <= 50) callback(null, list[3]);
-    else if (a > 50 & a <= 60) callback(null, list[4]);
 
-    else {
-        setTimeout(function () {
-            callback(null, list[5]);
-        }, 5000);
+
+
+
+
+    /*
+    //TASK-A: 
+    
+    Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+    MASALAN countLetter("e", "engineer") 3ni return qiladi.
+    
+    1. engineer => forEach || map || for
+    2. condition malumotlarni solishtirish
+    3. return qilish
+    4. result
+    
+    //SOLUTION
+    function countLetter(letter, word) {
+        let count = 0;
+        for (let i = 0; i < word.length; i++) {
+            if (word.charAt(i) === letter) {
+                count++;
+            }
+        } return count;
     }
-};
+    const result = countLetter("e", "engineer");
+    console.log(result); // Output : 3
+    */
 
-console.log("passed here 0");
-maslahatBering(25, (err, data) => {
-    if (err) console.error("Eror:", err);
-    else console.log(data);
-})
-console.log("passed here 1");
+
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yaxshi talaba boling", //0-20
+//     "togri boshliq tanlang va koproq xato qiling", //20-30
+//     "ozingizga ishlashni boshlang", //30-40
+//     "siz kuchli bolgan narsalarni qiling", //50-60
+//     "endi dam oling, foydasi yoq"
+// ];
+
+// //CALLBACK
+// function maslahatBering(a, callback) {
+//     if (typeof a !== 'number') callback("insert a number", null);
+//     else if (a <= 20) callback(null, list[0]);
+//     else if (a > 20 & a <= 30) callback(null, list[1]);
+//     else if (a > 30 & a <= 40) callback(null, list[2]);
+//     else if (a > 40 & a <= 50) callback(null, list[3]);
+//     else if (a > 50 & a <= 60) callback(null, list[4]);
+
+//     else {
+//         setTimeout(function () {
+//             callback(null, list[5]);
+//         }, 5000);
+//     }
+// };
+
+// console.log("passed here 0");
+// maslahatBering(25, (err, data) => {
+//     if (err) console.error("Eror:", err);
+//     else console.log(data);
+// })
+// console.log("passed here 1");
 
 
 // async function maslahatBering(a) {
