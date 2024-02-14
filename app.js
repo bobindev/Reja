8dbf4ab9b7627cd36b77b00b9bad2fad75202ec9
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-02-15 01:45:17 +0900

    feat: FrontEnd javascriptda DELETE operatsiyasini qurish

--- app.js
+++ app.js
@@ -1,69 +1,109 @@
-console.log("Starting Web Server");
 
-const express = require('express');
+
+console.log("Web Serverni boshlash");
+const express = require("express");
+const res = require("express/Lib/response")
 const app = express();
 
-const fs = require("fs");
+/* const fs = require("fs");
 
 let user;
-fs.readFile('database/user.json', "utf8", (err, data) => {
-    if (err) {
-        console.log("ERROR: ", err);
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err){
+       console.log("ERROR", err); 
     } else {
         user = JSON.parse(data);
     }
-})
-//MongoDB connect
-const db = require('./server').db();
+}) */
 
-//1 Kirish code -  express ga kirib kelishga bog'liq kodlar
+// MongoDB chaqirish
+ const db = require("./server").db();
+ const mongodb = require("mongodb");
+
+// 1: Kirish code
 app.use(express.static("public"));
-app.use(express.json()); // kirib kelayotgan json formatdagi data ni object holatida bizga o'girib beradi
-app.use(express.urlencoded({ extended: true })); //html formdan post qilingan narsalarni express server qabul qilishi un xizmat qiladi
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
-//2: Session
-
-//3 View codes (ejs- backendda frontendni yasash uchun xizmat qiladigan muhit  (npm i ejs)). 
-
+// 2 Session code
+// 3 views code 
 app.set("views", "views");
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-//4 - Routing codes
+// 4: Routing code
+
 app.post("/create-item", (req, res) => {
-    console.log("user entered / creat item");
-    console.log(req.body);
-    const new_reja = req.body.reja
-    db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-        console.log(data.ops);
-       res.json(data.ops[0]);
-        // if (err) {
-        //     console.log(err);
-        //     res.end('something went wrong');
-        // } else {
-        //     res.end('successfully added');
-        // }
+      // TODO: code with db here
+ 
+      console.log('user entered /create-items');
+      const new_reja = req.body.reja;
+      db.collection("plans").insertOne({ reja: new_reja}, (err, data) => {
+         console.log(data.ops);
+        res.json(data.ops[0]);  
+        
+        /*      if(err) {
+            console.log(err);
+            res.end("something went wrong");
+         } else {
+            res.end("successfully added");
+         } */
+
+      });
+  
+});
+
+    app.post("/delete-item", (req, res) => {
+          const id = req.body.id;
+        //   console.log(id);
+        //  res.end("done"); 
+          db.collection("plans").deleteOne(
+            {_id: new mongodb.ObjectId(id) },
+             function(err, data) {
+            res.json({state: "success"})
+          });
     });
-    // code with db here
-});
 
-app.get("/author", (req, res) => {
+    app.post("/edit-item", (req, res) => {
+        const data = req.body;
+        console.log(data);
+        db.collection("plans").findOneAndUpdate(
+            {_id: new mongodb.ObjectId(data._id)}, 
+            {$set: {reja: data.new_input}}, function(err, data){
+            res.json({state: "success"});
 
-    res.render("author", { user: user })
-})
-app.get("/", function (req, res) {
-    console.log("user entered /");
-    db.collection("plans")
-        .find()
-        .toArray((err, data) => {
-            if (err) {
-                console.log(err);
-                res.end("Something went wrong!");
-            } else {
-
-                res.render("reja", { items: data });
-            }
         });
+        // res.end("done");
+    });
 
+/* app.get("/author", (req, res) => {
+    res.render("author", { user: user });
+}); */
+
+app.post("/delete-all", (req, res) => {
+    if(req.body.delete_all) {
+        db.collection("plans").deleteMany(function () {
+            res.json({state: "Hamma rejalar ochirildi"});
+        });
+    }
 });
+
+app.get("/", function (req, res) {
+    console.log('user entered /');
+    db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+        if(err) {
+            console.log(err);
+            res.end("Something went wrong");
+        } else {
+            console.log(data);
+            res.render("reja", { items: data});
+        }
+    });
+
+
+ 
+});
+
 
 module.exports = app;
--- public/browser.js
+++ public/browser.js
@@ -1,17 +1,21 @@
+// const { response } = require("../app");
+
 console.log('FrontEnd JS ishga tushdi');
 
 function itemTemplate(item) {
-    return ` <li class="list-group-item list-group-item-info d-flex justify-content-between align-items-center">
-        <span class="item-text">
-            ${item.reja}
-        </span>
-        <div>
-            <button data-id="${item._id}" class="edit-me btn btn-danger btn-sm mr-1">
-                O'chirish</button>
-            <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">
-                O'chirish</button>
-        </div>
-    </li>`;
+    return ` <li class="list-group-item list-group-item-info d-flex align-items-center justify-content-between">
+    <span class="item-text">
+    ${item.reja}
+    </span>
+    <div>
+        <button data-id="${item._id}" class="edit-me btn-secondary btn-sm mr-1">
+            Ozgarrtirish
+        </button>
+        <button data-id="${item._id}" class="delete-me btn btn-danger mr-1">
+            Ochirish
+        </button>
+    </div>
+</li> `;
 }
 
 let createField = document.getElementById("create-field")
@@ -21,11 +25,11 @@ document
     .addEventListener("submit", function (e) {
         e.preventDefault();
         axios
-            .post('/create-item', { reja: createField.value })
+            .post("/create-item", { reja: createField.value })
             .then((response) => {
                 document
                 .getElementById("item-list")
-                .insertAdjacentElement("beforeend", itemTemplate(response.data));
+                .insertAdjacentHTML("beforeend", itemTemplate(response.data));
                 createField.value = "";
                 createField.focus();
             })
@@ -33,3 +37,27 @@ document
                 console.log("Iltimos qaytadan harakat qiling");
             });
     });
+
+    document.addEventListener("click", function (e) {
+        //delete oper
+
+        console.log(e.target);
+        if (e.target.classList.contains("delete-me")) {
+            if (confirm ("Aniq ochirmoqchimisiz?")) {
+                axios
+                .post("/delete-item", {id: e.target.getAttribute("data-id")})
+                .then((response) => {
+                    console.log(response.data);
+                    e.target.parentElement.parentElement.remove();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                  });
+            };
+        }
+
+        if (e.target.classList.contains("edit-me")) {
+            alert ("siz edit tugmasini bosdingiz")
+        }
+    }); 
+
